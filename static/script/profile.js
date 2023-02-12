cbebea447cd9c80d809389788bac02c1928f4c0a
Saif Ali <89186408+SaifAliii@users.noreply.github.com>
2023-02-13 00:07:50 +0500

    Add files via upload

--- static/script/profile.js
+++ static/script/profile.js
@@ -1,3 +1,28 @@
+let myData;
+$.ajax({
+    type: "GET",
+    url: '/api/Users',
+    success: function(result){
+        myData = result
+        console.log(result.length)
+        for(i = 0; i < 8; i++){
+            var imgpath = result[i]["profilePic"];
+            document.getElementById(i+1).src = imgpath;
+            let className = ".user" + (i + 1);
+            console.log(className)
+            document.querySelector(className).innerHTML = result[i]["firstName"] + " " + result[i]["lastName"];
+        }
+        var img = document.querySelector("#profile-img");
+        img.src = result[10]["profilePic"];
+        console.log("done")
+    },
+    error: function(xhr, resp, text){
+        console.log(xhr.status, resp, text);
+    }
+})
+
+
+
 var x = document.getElementById("container-img");
 x.addEventListener("mouseover", myFunction);
 x.addEventListener("mouseout", myFunction1);
